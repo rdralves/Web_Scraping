d954d7a9c446c62318c61faed65a1bf7913f8811
rdralves <rdr.alves@gmail.com>
2023-05-04 15:18:07 -0300

    Instanciando o selenium, definindo navegador e abrindo página, onde se fará webscraping

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+
+# Instanciando o selenium, definindo navegador e abrindo página, onde se fará a raspagem de dados
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
