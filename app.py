643af8f6504e1dddb08aef2716db6a3248da1197
rdralves <rdr.alves@gmail.com>
2023-05-04 15:25:21 -0300

    Abrindo um site com selenium

--- app.py
+++ app.py
@@ -4,3 +4,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 # Instanciando o selenium, definindo navegador e abrindo página, onde se fará a raspagem de dados
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+# Abrindo um site com Selenium
+driver.get('https://sitepreco1.netlify.app/')
