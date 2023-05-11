02d70cb0c8894f7c9fbb6cfb08cd50b7895c932f
rdralves <rdr.alves@gmail.com>
2023-05-11 18:17:52 -0300

    fazendo as importações corretas e finalizando projeto

--- projeto2/app.py
+++ projeto2/app.py
@@ -1,21 +1,40 @@
 # Importação da biblioteca Selenium
 from selenium import webdriver
-import os 
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+import os
 
-os.system("cls")
+class RoboFormulario:
 
-# Instanciando o Selenium
-url = webdriver.Chrome()
+    dados = ['rdr@gmail.com', 12365498798, 'rdr123']
+    os.system("cls")
+    print("Iniciando nosso robô...")
 
-# abrindo a página
-url.get('https://rdralves.github.io/home/')
+    # Instanciando o Selenium
+    url = webdriver.Chrome()
 
-# preencher email
-url.find_element('//*[@id="exampleInputEmail1"]').send_keys('rdr@gmail.com')
-# preecher cpf
+    # abre página
+    url.get('https://rdralves.github.io/home/')
 
-# peencher senha
+    # preencher email
+    url.find_element(By.XPATH, '//*[@id="exampleInputEmail1"]').send_keys(dados[0])
+    time.sleep(0.5)
 
-# clicar no botão Enviar
+    # preenche cpf
+    url.find_element(By.XPATH, '//*[@id="exampleInputCPF"]').send_keys(dados[1])
+    time.sleep(0.5)
 
-input('qualquer coisa...')
+    # peenche senha
+    url.find_element(
+        By.XPATH, '//*[@id="exampleInputPassword1"]').send_keys(dados[2])
+    time.sleep(0.5)
+
+    # clica no botão Enviar
+    url.find_element(By.XPATH, '/html/body/div/form/button').click()
+    time.sleep(0.5)
+
+    # finalizado o robo
+    print('Missão cumprinda...')
