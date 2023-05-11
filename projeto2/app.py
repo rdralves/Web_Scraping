7e8547018b3db157c55708078760f19f6760b9eb
rdralves <rdr.alves@gmail.com>
2023-05-11 18:18:41 -0300

    projeto finalizado

--- projeto2/app.py
+++ projeto2/app.py
@@ -7,34 +7,33 @@ from webdriver_manager.chrome import ChromeDriverManager
 import time
 import os
 
-class RoboFormulario:
 
-    dados = ['rdr@gmail.com', 12365498798, 'rdr123']
-    os.system("cls")
-    print("Iniciando nosso robô...")
+dados = ['rdr@gmail.com', 12365498798, 'rdr123']
+os.system("cls")
+print("Iniciando nosso robô...")
 
-    # Instanciando o Selenium
-    url = webdriver.Chrome()
+# Instanciando o Selenium
+url = webdriver.Chrome()
 
-    # abre página
-    url.get('https://rdralves.github.io/home/')
+# abre página
+url.get('https://rdralves.github.io/home/')
 
-    # preencher email
-    url.find_element(By.XPATH, '//*[@id="exampleInputEmail1"]').send_keys(dados[0])
-    time.sleep(0.5)
+# preencher email
+url.find_element(By.XPATH, '//*[@id="exampleInputEmail1"]').send_keys(dados[0])
+time.sleep(0.5)
 
-    # preenche cpf
-    url.find_element(By.XPATH, '//*[@id="exampleInputCPF"]').send_keys(dados[1])
-    time.sleep(0.5)
+# preenche cpf
+url.find_element(By.XPATH, '//*[@id="exampleInputCPF"]').send_keys(dados[1])
+time.sleep(0.5)
 
-    # peenche senha
-    url.find_element(
-        By.XPATH, '//*[@id="exampleInputPassword1"]').send_keys(dados[2])
-    time.sleep(0.5)
+# peenche senha
+url.find_element(
+    By.XPATH, '//*[@id="exampleInputPassword1"]').send_keys(dados[2])
+time.sleep(0.5)
 
-    # clica no botão Enviar
-    url.find_element(By.XPATH, '/html/body/div/form/button').click()
-    time.sleep(0.5)
+# clica no botão Enviar
+url.find_element(By.XPATH, '/html/body/div/form/button').click()
+time.sleep(0.5)
 
-    # finalizado o robo
-    print('Missão cumprinda...')
+# finalizado o robo
+print('Missão cumprinda...')
